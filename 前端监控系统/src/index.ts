3214452c0fc9abc29ce78e266bed194643b75d72
xiafengkun <521320930@qq.com>
2020-11-10 09:43:22 +0800

    X: 更新  Vue 文档

---
+++ 前端监控系统/src/index.ts
@@ -0,0 +1,22 @@
+
+function Ealge (option) {
+    this.url = option.url
+    this.appId = option.appId
+}
+
+Ealge.prototype.init = function () {
+    
+}
+
+Ealge.prototype.jsError = function () {
+
+}
+
+Ealge.prototype.resourceError = function () {
+
+}
+
+Ealge.prototype.requestError = function () {
+
+}
+
