6c7d7f79b06418ec015653680e8d6daf5b680392
xiafengkun <521320930@qq.com>
2021-02-19 16:13:44 +0800

    T: Promise

--- 方案/0.顺序执行.ts
+++ 方案/0.顺序执行.ts
@@ -1,2 +1,18 @@
 
-export default function
+export default function pick (p: Array<Promise<any>>) {
+  // let i = 0
+  // if (p.length == 0) return []
+  // function step () {
+  //   p[i].then(() => {
+  //     i++
+  //     if (i <= p.length) {
+  //       step()
+  //     }
+  //   })
+  // }
+  // step()
+
+  const step = (p, next) => { if (p) p.then(() => next()) }
+  const run = () => step(p.shift(), run)
+  return run()
+}
--- 方案/1.并发做异步请求，限制频率.ts
+++ 方案/1.并发做异步请求，限制频率.ts
@@ -4,8 +4,26 @@
  * @param max 
  * @param callback 
  */
-export default function sendRequest (urls: Array<Promise<any>>, max: number, callback: () => void): void {
-  const queue = []
-  
+export default function sendRequest (p: Array<Promise<any>>, max: number, callback: () => void): void {
+  if (p.length === 0) return callback && callback()
+  const am = p.splice(0, max)
 
+  for (let i = 0; i < am.length; i++) {
+    am[i] = Promise.resolve(loadImg(am[i]))
+  }
+
+  Promise.all(am).then(() => {
+    sendRequest(p, max, callback)
+  })
+}
+function loadImg(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = (e) => {
+      console.log('一张图片加载完成', url);
+      resolve(e);
+    };
+    img.onerror = reject;
+    img.src = url;
+  });
 }
